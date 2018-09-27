950921d7e9522725ae35912ba31c65fa53a9c1cf
Reiss Cashmore <reiss.cashmore@outlook.com>
2018-09-27 16:03:57 +0100

    Add auth temporary component

---
+++ client/views/Auth.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div id="firebaseui-auth-container"></div>
+</template>
+<script>
+  import AuthService from "services/auth.service.js";
+  let _authService = new AuthService();
+export default {
+  name: 'auth',
+  mounted() {
+   _authService.createAccountWithEmail();
+    }
+}
+</script>
