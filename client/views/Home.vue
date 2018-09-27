65052988e2c025e80337c79e1c08794f97450351
Reiss Cashmore <reiss.cashmore@outlook.com>
2018-09-27 16:03:10 +0100

    Homepage view components and styling additions

--- client/views/Home.vue
+++ client/views/Home.vue
@@ -1,47 +1,6 @@
 <template>
-  <div class="page-container">
-    <md-app>
-      <md-app-toolbar class="md-primary" md-elevation="0">
-        <md-button class="md-icon-button" @click="toggleMenu" v-if="!menuVisible">
-          <md-icon>menu</md-icon>
-        </md-button>
-        <span class="md-title">Up To Lists</span>
-      </md-app-toolbar>
-
-
-      <md-app-drawer :md-active.sync="menuVisible" md-persistent="mini">
-        <md-toolbar class="md-transparent" md-elevation="0">
-          <span>Navigation</span>
-
-          <div class="md-toolbar-section-end">
-            <md-button class="md-icon-button md-dense" @click="toggleMenu">
-              <md-icon>keyboard_arrow_left</md-icon>
-            </md-button>
-          </div>
-        </md-toolbar>
-
-        <md-list>
-          <md-list-item>
-            <md-icon>move_to_inbox</md-icon>
-            <span class="md-list-item-text">Inbox</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>send</md-icon>
-            <span class="md-list-item-text">Sent Mail</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>delete</md-icon>
-            <span class="md-list-item-text">Trash</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>error</md-icon>
-            <span class="md-list-item-text">Spam</span>
-          </md-list-item>
-        </md-list>
-      </md-app-drawer>
+  <div class="page-container" >
+    <md-toolbar-small title="Up To Lists">
 
       <md-app-content>
         <div class="md-layout md-gutter md-alignment-center">
@@ -54,11 +13,9 @@
             
           </list-summary>
         </div>
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error quibusdam, non molestias et! Earum magnam,
-        similique, quo recusandae placeat dicta asperiores modi sint ea.
       </md-app-content>
-    </md-app>
+
+    </md-toolbar-small>
   </div>
 </template>
 
@@ -66,21 +23,18 @@
   import ListSummary from "components/ListSummary";
   import ListsService from "services/list.service.js";
   let _listsService = new ListsService();
+
+  import MdToolbarSmall from "components/MdToolbarSmall"
   export default {
     components: {
-      ListSummary
+      ListSummary,
+      MdToolbarSmall
     },
     data: () => ({
-      menuVisible: false,
       lists: []
     }),
     methods: {
-      toggleMenu() {
-        this.menuVisible = !this.menuVisible;
-      },
-      log(){
-        console.log(this.lists)
-      }
+
     },
     firestore: {
       lists: _listsService.getLists()
@@ -88,20 +42,8 @@
   };
 
 </script>
-<style lang="scss" scoped>
-  .md-app {
-    min-height: 100vh;
-    border: 1px solid rgba(#000, 0.12);
-  }
+<style lang="scss" >
 
-  .md-toolbar {
-    min-height: 64px;
-  }
 
-  // Demo purposes only
-  .md-drawer {
-    width: 230px;
-    max-width: calc(100vw - 125px);
-  }
-
+ 
 </style>
