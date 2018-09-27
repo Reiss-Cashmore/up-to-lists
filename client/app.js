68982974dbc739131f5a18144c352a3d532d1e2b
Reiss Cashmore <reiss.cashmore@outlook.com>
2018-09-27 16:05:58 +0100

    Add line clamping package and component Globally register new toolbar components

--- client/app.js
+++ client/app.js
@@ -15,8 +15,16 @@ import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 Vue.use(VueMaterial)
 
+import lineClamp from 'vue-line-clamp'
+Vue.use(lineClamp, {})
+
 sync(store, router)
 
+import MdToolbarTall from './components/MdToolbarTall'
+import MdToolbarSmall from './components/MdToolbarSmall'
+ 
+Vue.component('md-toolbar-tall', MdToolbarTall)
+Vue.component('md-toolbar-small', MdToolbarSmall)
 
 const app = new Vue({
   router,
