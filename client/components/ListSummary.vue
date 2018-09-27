1ef4a4d7a2e0c862beef073b90bb6dfe5c7be6ba
Reiss Cashmore <reiss.cashmore@outlook.com>
2018-09-27 16:05:06 +0100

    Changes to list summary component

--- client/components/ListSummary.vue
+++ client/components/ListSummary.vue
@@ -1,22 +1,32 @@
 <template>
-  <div v-on:click="log">
-    <md-card class="md-accent" md-with-hover>
+  <div v-on:click="viewList">
+
+    <md-card   class="md-accent" md-with-hover>
       <md-ripple>
 
         <md-card-header>
+          <md-icon>thumb_up</md-icon> {{list.upvotes}}
           <div class="md-title">{{list.name}}</div>
-          <div class="md-subhead">With hover and ripple effects</div>
+          <div class="md-subhead">{{list.summary}}</div>
+
         </md-card-header>
 
-        <md-card-content v-for="(listItem, index) in listItems" :key="index">
-          <span class="votes">
-          <md-icon>thumb_up</md-icon>  {{listItem.upvotes - listItem.downvotes}}
-          </span> {{listItem.title}}
+        <md-card-content>
+          <md-table class="md-dense" v-model="listItems">
+            <md-table-row slot="md-table-row" slot-scope="{ item }">
+              <md-table-cell md-label="Chromebook">{{ item.title }}</md-table-cell>
+              <md-table-cell md-label="Votes" md-numeric>{{ item.upvotes - item.downvotes }}</md-table-cell>
+
+            </md-table-row>
+          </md-table>
+
+          <span v-if="listItems - 7 > 0" class="md-caption pull-right">{{listItems.length - 7}} more entries</span>
+
         </md-card-content>
 
         <md-card-actions>
-          <md-button>Action</md-button>
-          <md-button>Action</md-button>
+          <md-button>Add entry</md-button>
+          <md-button>Save</md-button>
         </md-card-actions>
       </md-ripple>
     </md-card>
@@ -28,13 +38,13 @@
   let _listsService = new ListsService();
   export default {
     name: "list-summary",
-    props: ["list"],
+    props: ["list", "to"],
     data: () => ({
       listItems: []
     }),
     methods: {
-      log() {
-        console.log(this.list.id);
+      viewList: function () {
+        this.$router.push('list/' + this.list.id)
       }
     },
     mounted() {
@@ -47,5 +57,17 @@
 
 <style lang="scss" scoped>
   .list-item-votes {}
+
+  .md-content.md-theme-default {
+    background-color: transparent;
+
+    &.md-dense {}
+  }
+
+  .md-table {
+    .md-table-cell {
+      height: 40px;
+    }
+  }
 
 </style>
